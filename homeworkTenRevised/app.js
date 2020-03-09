4692b5cd478510b2c8179796c92516fc6117e731
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-03-08 21:08:57 -0600

    start code for team profile generator

---
+++ homeworkTenRevised/app.js
@@ -0,0 +1,10 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const outputPath = path.resolve(__dirname, "output", "team.html");
+
+const render = require("./lib/htmlRenderer");
